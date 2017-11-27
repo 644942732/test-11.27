9cc0ed8f948c62f4d3941289e439aceda80e5151
644942732@qq.com <644942732>
2017-11-27 19:28:25 +0800

    om

--- src/main/java/com/example/demo/OmApplication.java
+++ src/main/java/com/example/demo/OmApplication.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,23 +11,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootApplication
 public class OmApplication {
 
+
+	private static TranBean bean;
+
 	@Autowired
-	private TranBean bean;
-
-	private static OmApplication omApplication;
-
-	@PostConstruct
-	public void init() {
-		omApplication = this;
-		omApplication.bean = this.bean;
+	public void setBean(TranBean bean) {
+		this.bean = bean;
 	}
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		SpringApplication.run(OmApplication.class, args);
+		ApplicationContext context = SpringApplication.run(OmApplication.class, args);
 
-		String json = omApplication.bean.objectToJson(new Person("1", 2));
+		String json = bean.objectToJson(new Person("1", 2));
 		System.out.println(json);
-		Person p = (Person) omApplication.bean.jsontoObject(json, Person.class);
+		Person p = (Person) bean.jsontoObject(json, Person.class);
 		System.out.println(p);
 	}
 
